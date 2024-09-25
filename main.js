1350136fafd032aa099f55f28fe35103f7290040
AlexisLuna <alexisgabriel.lu@gmail.com>
2024-09-25 18:14:55 -0500

    add pag ubicacion

---
+++ main.js
@@ -0,0 +1,53 @@
+// Seccion Menu Hamburguesa
+// Obtén el botón dropdown
+var dropdown = document.querySelector('.dropdown');
+
+// Agrega un evento de clic al botón dropdown
+dropdown.addEventListener('click', function(event) {
+    // Obtén el contenido del dropdown
+    var dropdownContent = this.querySelector('.dropdown-content');
+    // Alterna la visibilidad del contenido del dropdown
+    dropdownContent.style.display = dropdownContent.style.display === 'block' ? 'none' : 'block';
+});
+
+//Termina  Seccion Menu Hamburguesa
+
+// Seccion Firebase
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB3AJBzqkZE8zVLZ8gJAVRkOeLyBl1aKao",
+    authDomain: "csmmobiliario.firebaseapp.com",
+    databaseURL: "https://csmmobiliario-default-rtdb.firebaseio.com",
+    projectId: "csmmobiliario",
+    storageBucket: "csmmobiliario.appspot.com",
+    messagingSenderId: "465303066360",
+    appId: "1:465303066360:web:a46b8d62debb65b78b4d0d",
+};
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+var formulario = document.getElementById('Formu');
+const saveCliente = (nombre, telefono, mail, mensaje) =>
+    db.collection("Cliente").doc().set({
+        nombre,
+        telefono,
+        mail,
+        mensaje
+    })
+formulario.addEventListener("submit", async(e) => {
+        e.preventDefault();
+        const nombre = formulario["Nombre"];
+        const telefono = formulario["Telefono"];
+        const mail = formulario["Mail"];
+        const mensaje = formulario["Mensaje"];
+        try {
+            /* console.log(nombre, telefono, mail, mensaje); */
+            await saveCliente(nombre.value, telefono.value, mail.value, mensaje.value);
+            alert('La información ha sido enviada correctamente');
+            nombre.focus();
+            formulario.reset();
+        } catch (error) {
+            alert("no ha sido posible enviar los datos");
+        }
+
+    })
+    // Termina la seccion Firebase
